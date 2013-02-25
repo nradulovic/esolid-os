892c1084cc228f19f86bfe367d8cdb7b0af17863
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-02-25 19:26:23 +0000

    - sređivanje doxygen komentara - više ne postoje Ctor i init funkcije memorijskog menadzera

--- inc/kernel/mm.h
+++ inc/kernel/mm.h
@@ -36,17 +36,6 @@
 /*============================================================================  INCLUDE FILES  ==*/
 /*==================================================================================  DEFINES  ==*/
 /*==================================================================================  MACRO's  ==*/
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Rezervise memorijski prostor za koriscenje od strane dinamickog
- *              memorijskog alokatora (hmem)
- * @param       size                    Zeljena velicina dinamicke memorije u
- *                                      bajtovima.
- * @attention   Makro se mora pozvati pre upotrebe bilo koje funkcija iz MM
- *              modula.
- *//*--------------------------------------------------------------------------------------------*/
-#define ES_MEMORY_DEFINE(name, size)                                            \
-    static C_ALIGNED(ES_CPU_ATTRIB_ALIGNMENT) uint8_t name[ES_ALIGN(size, ES_CPU_ATTRIB_ALIGNMENT)]
-
 /*-------------------------------------------------------------------------  C++ extern begin  --*/
 #ifdef __cplusplus
 extern "C" {
@@ -54,53 +43,34 @@ extern "C" {
 
 /*===============================================================================  DATA TYPES  ==*/
 /*=========================================================================  GLOBAL VARIABLES  ==*/
+
 /*-------------------------------------------------------------------------------------------*//**
  * @name        Klase memorijskog alokatora
  * @{ *//*---------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dinamicki memorijski alokator (heap memory)
- *//*--------------------------------------------------------------------------------------------*/
+ */
 extern const C_ROM esMemClass_T memHeapClass;
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Skladiste memorije (memory pool)
- *//*--------------------------------------------------------------------------------------------*/
+ */
 extern const C_ROM esMemClass_T memPoolClass;
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Staticki memorijski alokator (static memory)
- *//*--------------------------------------------------------------------------------------------*/
+ */
 extern const C_ROM esMemClass_T memStaticClass;
 /** @} *//*--------------------------------------------------------------------------------------*/
 
 /*======================================================================  FUNCTION PROTOTYPES  ==*/
+
 /*-------------------------------------------------------------------------------------------*//**
  * @name        Funkcije statickog memorijskog alokatora
  * @{ *//*---------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Inicijalizuje memorijski alokator sa memorijom @c aHeap
- *              velicine @c aSize
- * @param       aHeap                   Pokazivac na unapred rezervisan
- *                                      memorijski prostor,
- * @param       aSize                   velicina memorijskog prostora u
- *                                      bajtovima.
- * @note        Nakon inicijalizacije memorijskog prostora, slobodna memorija
- *              nece biti jednaka aHeap velicini vec ce biti umanjena za iznos
- *              koji priblizno odgovara statickom zauzecu alokatora koji se
- *              koristi. Na primer, first fit alokator ce umanjiti vrednost
- *              slobodne memorije za 24B u Cortex-M3 portu.
- * @details     Pre pozivanja ove funkcije potrebno je izvrsiti rezervaciju
- *              memorijskog prostora. To se vrsi upotrebom
- *              @ref ES_MEMORY_DEFINE makroa.
- * @notapi
- *//*--------------------------------------------------------------------------------------------*/
-void esSmemCtor(
-    void        * aHeap,
-    size_t      aSize);
-
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dodeljuje memorijski prostor velicine @c aSize
  * @param       aSize                   Velicina zahtevanog memorijskog prostora
  *                                      u bajtovima.
@@ -113,25 +83,24 @@ void esSmemCtor(
  *              Kada se ne koristi debug rezim funkcija se ponasa u skladu sa
  *              standardom.
  * @api
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void * esSmemAlloc(
     size_t      aSize);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dodeljuje memorijski prostor velicine @c aSize
  * @param       aSize                   Velicina zahtevanog memorijskog prostora
  *                                      u bajtovima.
  * @pre         Najpre treba inicijalizovati MM modul pozivom
  *              @ref ES_MEMORY_DEFINE makroa.
- * @iclass
- *
- *              U debug rezimu ova funkcija uvek vraca pokazivac, odnosno, ne
+ * @details		U debug rezimu ova funkcija uvek vraca pokazivac, odnosno, ne
  *              moze se desiti da vrati NULL pokazivac, kao sto nalaze
  *              standardna implementacija @c malloc C funkcije. Ukoliko se
  *              zahtevana memorija ne moze dobaviti generisace se ASSERT greska.
  *              Kada se ne koristi debug rezim funkcija se ponasa u skladu sa
  *              standardom.
- *//*--------------------------------------------------------------------------------------------*/
+ * @iclass
+ */
 void * esSmemAllocI(
     size_t      aSize);
 
@@ -140,118 +109,75 @@ void * esSmemAllocI(
  * @name        Funkcije dinamickog memorijskog alokatora
  * @{ *//*---------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Inicijalizuje memorijski alokator sa memorijom @c aHeap
- *              velicine @c aSize
- * @param       aHeap                   Pokazivac na unapred rezervisan
- *                                      memorijski prostor,
- * @param       aSize                   velicina memorijskog prostora u
- *                                      bajtovima.
- * @note		Nakon inicijalizacije memorijskog prostora, slobodna memorija
- * 				nece biti jednaka aHeap velicini vec ce biti umanjena za iznos
- * 				koji priblizno odgovara statickom zauzecu alokatora koji se
- * 				koristi. Na primer, first fit alokator ce umanjiti vrednost
- * 				slobodne memorije za 24B u Cortex-M3 portu.
- * @api
- *
- *              Pre pozivanja ove funkcije potrebno je izvrsiti rezervaciju
- *              memorijskog prostora. To se moze izvrsiti na nekoliko nacina,
- *              ali se preporucuje metoda upotrebom MM_HEAP_MM_DEFINE makroa.
- *
- *              Primer rezervacije i inicijalizacije memorije velicine 1kB:
- *
- *              @code
- *              uint8_t mem_memory[1024];
- *
- *              esHmemInit((void *)&mem_memory, sizeof(mem_memory[1024]));
- *              @endcode
- *
- *              ili koristeci dinamicku memoriju:
- *
- *              @code
- *              uint8_t * pMemory;
- *
- *              pMemory = (uint8_t *)malloc(1024);
- *
- *              esHmemInit((void *)pMemory, 1024);
- *              @endcode
- *//*--------------------------------------------------------------------------------------------*/
-void esHmemInit(
-    void        * aHeap,
-    size_t      aSize);
-
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dodeljuje memorijski prostor velicine @c aSize
  * @param       aSize                   Velicina zahtevanog memorijskog prostora
  *                                      u bajtovima.
  * @pre         Najpre treba inicijalizovati MM modul pozivom
  *              @ref ES_MEMORY_DEFINE makroa.
- * @api
- *
- *              U debug rezimu ova funkcija uvek vraca pokazivac, odnosno, ne
+ * @details		U debug rezimu ova funkcija uvek vraca pokazivac, odnosno, ne
  *              moze se desiti da vrati NULL pokazivac, kao sto nalaze
  *              standardna implementacija @c malloc C funkcije. Ukoliko se
  *              zahtevana memorija ne moze dobaviti generisace se ASSERT greska.
  *              Kada se ne koristi debug rezim funkcija se ponasa u skladu sa
  *              standardom.
- *//*--------------------------------------------------------------------------------------------*/
+ * @api
+ */
 void * esHmemAlloc(
     size_t      aSize);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dodeljuje memorijski prostor velicine @c aSize
  * @param       aSize                   Velicina zahtevanog memorijskog prostora
  *                                      u bajtovima.
  * @pre         Najpre treba inicijalizovati MM modul pozivom
  *              @ref ES_MEMORY_DEFINE makroa.
- * @iclass
- *
- *              U debug rezimu ova funkcija uvek vraca pokazivac, odnosno, ne
+ * @details		U debug rezimu ova funkcija uvek vraca pokazivac, odnosno, ne
  *              moze se desiti da vrati NULL pokazivac, kao sto nalaze
  *              standardna implementacija @c malloc C funkcije. Ukoliko se
  *              zahtevana memorija ne moze dobaviti generisace se ASSERT greska.
  *              Kada se ne koristi debug rezim funkcija se ponasa u skladu sa
  *              standardom.
- *//*--------------------------------------------------------------------------------------------*/
+ * @iclass
+ */
 void * esHmemAllocI(
     size_t      aSize);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Reciklira memorijski prostor na koji pokazije @c aMemory
  *              pokazivac
  * @param       aMemory                 Pokazivac na prethodno dodeljen
  * 										memorijski prostor.
  * @api
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void esHmemDeAlloc(
     void        * aMemory);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Reciklira memorijski prostor na koji pokazije @c aMemory
  *              pokazivac
  * @param       aMemory                 Pokazivac na prethodno dodeljen
  *                                      memorijski prostor.
  * @iclass
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void esHmemDeAllocI(
     void        * aMemory);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Vraca velicinu trenutno slobodne memorije u bajtovima.
  * @return      Velicina slobodne memorije u bajtovima.
  * @pre         Najpre treba inicijalizovati MM modul pozivom
  *              @ref ES_MEMORY_DEFINE makroa.
- * @api
- *
- *              Ukoliko je memorija jako fragmenitisana, sto je karakteristicno
+ * @details		Ukoliko je memorija jako fragmenitisana, sto je karakteristicno
  *              za first fir algoritam, moze se desiti da postoji dovoljno
  *              slobodne memorije, ali ne i bloka zahtevane velicine. U tom
  *              slucaju ce memorijski alokator pasti.
- *//*--------------------------------------------------------------------------------------------*/
+ * @api
+ */
 size_t esHmemFreeSpace(
     void);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Vraca velicinu trenutno slobodne memorije u bajtovima.
  * @return      Velicina slobodne memorije u bajtovima.
  * @pre         Najpre treba inicijalizovati MM modul pozivom
@@ -262,11 +188,11 @@ size_t esHmemFreeSpace(
  *              za first fir algoritam, moze se desiti da postoji dovoljno
  *              slobodne memorije, ali ne i bloka zahtevane velicine. U tom
  *              slucaju ce memorijski alokator pasti.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 size_t esHmemFreeSpaceI(
     void);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief		Vraca velicinu dodeljene memorije
  * @param 		aMemory					Pokazivac na prethodno dodeljen
  * 										memorijski prostor.
@@ -278,7 +204,7 @@ size_t esHmemFreeSpaceI(
  *              se dodeliti veca memorija od zahtevane. Ova funkcija se moze
  *              koristiti ukoliko je potrebna informacija o stvarnoj velicini
  *              dodeljenog bloka.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 size_t esHmemBlockSize(
 	void 		* aMemory);
 
@@ -287,7 +213,7 @@ size_t esHmemBlockSize(
  * @name        Ostale operacije sa memorijom
  * @{ *//*---------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Vrsi kopiranje @c aLength bajtova sa izvorne adrese @c aSrc na
  *              odredisnu adresu @c aDst.
  * @param       aDst                    Pokazivac na odredisnu adresu,
@@ -300,13 +226,13 @@ size_t esHmemBlockSize(
  *              kopirati blokove cije se adrese preklapaju treba koristiti
  *              funkciju esMemMove().
  * @api
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void * esMemCopy(
     void        * C_RESTRICT aDst,
     const void  * C_RESTRICT aSrc,
     size_t      aLength);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Vrsi kopiranje @c aLength bajtova sa izvorne adrese @c aSrc na
  *              odredisnu adresu @c aDst.
  * @param       aDst                    Pokazivac na odredisnu adresu,
@@ -315,7 +241,7 @@ void * esMemCopy(
  * @note        Za razliku od esMemMove() ova funkcija moze se koristiti u svim
  *              slucajevima, jer se ne primenjuju optimizacione tehnike.
  * @api
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void * esMemMove(
     void        * aDst,
     const void  * aSrc,
