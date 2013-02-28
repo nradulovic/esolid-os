3514ef77639aaf9be9a05f784ceb03f024a3f502
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-02-28 19:21:25 +0000

    - doxygen komentari - dynamic član evtHeader strukture je sada uvek tipa uint_fast8_t, više ne postoji konfiguracija njegovog tipa - izbačeni su svi typedef-ovi - izbačena je struktura evtIntr, podaci su direktno ugrađeni u esEvtHeader - Dodat je veći broj internih funkcija koje direktno pristupaju redu za cekanje

--- src/kernel/evt_pkg.h
+++ src/kernel/evt_pkg.h
@@ -20,51 +20,19 @@
  *
  * web site:    http://blueskynet.dyndns-server.com
  * e-mail  :    blueskyniss@gmail.com
- *************************************************************************************************/
-
-
-/*********************************************************************************************//**
+ *//******************************************************************************************//**
  * @file
- *
- * @author  	Nenad Radulovic
- *
+ * @author      Nenad Radulovic
  * @brief       Privatni interfejs Event Object podmodula.
- *
- * ------------------------------------------------------------------------------------------------
- *
  * @addtogroup  evt_impl
- *
  ****************************************************************************************//** @{ */
-
 
 #ifndef EVT_PKG_H_
 #define EVT_PKG_H_
 
-
-/*************************************************************************************************
- * INCLUDE FILES
- *************************************************************************************************/
-
-
-/*-----------------------------------------------------------------------------------------------*
- * EXTERNS
- *-----------------------------------------------------------------------------------*//** @cond */
-
-#ifdef EVT_PKG_H_VAR
-#define EVT_PKG_H_EXT
-#else
-#define EVT_PKG_H_EXT extern
-#endif
-
-
-/** @endcond*//***********************************************************************************
- * DEFINES
- *************************************************************************************************/
-
-
-/*************************************************************************************************
- * MACRO's
- *************************************************************************************************/
+/*============================================================================  INCLUDE FILES  ==*/
+/*==================================================================================  DEFINES  ==*/
+/*==================================================================================  MACRO's  ==*/
 
 /*-------------------------------------------------------------------------------------------*//**
  * @name        Debug podrska
@@ -90,45 +58,32 @@
 
 /** @} *//*--------------------------------------------------------------------------------------*/
 
-
-/*-----------------------------------------------------------------------------------------------*
- * C/C++ #ifdef - open
- *-----------------------------------------------------------------------------------------------*/
-#ifdef __cplusplus
+/*-------------------------------------------------------------------------  C++ extern begin  --*/
+#if defined(__cplusplus)
 extern "C" {
 #endif
 
+/*===============================================================================  DATA TYPES  ==*/
+/*=========================================================================  GLOBAL VARIABLES  ==*/
 
-/*************************************************************************************************
- * DATA TYPES
- *************************************************************************************************/
+extern const C_ROM esEvtHeader_T evtSignal[];
 
+/*======================================================================  FUNCTION PROTOTYPES  ==*/
 
-/*************************************************************************************************
- * GLOBAL VARIABLES
- *************************************************************************************************/
-
-
-extern const C_ROM evtIntr_T evtSignal[];
-
-/*************************************************************************************************
- * FUNCTION PROTOTYPES
- *************************************************************************************************/
-
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Vraca da li je red cekanja za dogadjaje prazan
  * @param       aEpa                    Pokazivac na EPA objekat
  * @return      Status reda za cekanje.
  *  @retval     TRUE - red za cekanje je prazan.
  *  @retval     FALSE - red za cekanje nije prazan.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 C_INLINE_ALWAYS bool_T evtQIsEmpty_(
     esEpaHeader_T       * aEpa) {
 
-    return (esQpIsEmpty(&(aEpa->internals.evtQueue.queue)));
+    return (esQpIsEmpty_(&(aEpa->evtQueue.queue)));
 }
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Unistava dogadjaj.
  * @param       aEvt                    Pokazivac na dogadjaj koji treba da se
  *                                      unisti.
@@ -136,112 +91,106 @@ C_INLINE_ALWAYS bool_T evtQIsEmpty_(
  *              onda ce memorijski prostor koji on zauzima biti recikliran, u
  *              suprotnom, dogadjaj nastavlja da postoji.
  * @iclass
- *//*--------------------------------------------------------------------------------------------*/
+ */
 C_INLINE_ALWAYS void evtDestroyI_(
     esEvtHeader_T       * aEvt) {
 
-    if ((evtDynamic_T)0U == aEvt->internals.dynamic) {
+    if ((uint_fast8_t)0U == aEvt->dynamic) {
 
 #if defined(OPT_KERNEL_DBG_EVT) && defined(OPT_DBG_USE_CHECK)
-        aEvt->internals.signature = 0xDEAD;
+    aEvt->signature = ~EVT_SIGNATURE;                                           /* Postavljanje lošeg potpisa                               */
 #endif
         esHmemDeAllocI((void *)aEvt);
     }
 }
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Vraca kolika je potrebna velicina memorijskog prostora za
  *              cuvanje bafera dogadjaja.
- * @param       aQueueSize              Maksimalna dogadjaja u baferu.
+ * @param       aQueueSize              Maksimalan broj dogadjaja u baferu.
  * @return      Potreban memorijski prostor u bajtovima.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 C_INLINE_ALWAYS size_t evtQReqSize_(
     size_t              aQueueSize) {
 
     return (aQueueSize * sizeof(void *));
 }
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Konstruise red cekanja za dogadjaje.
  * @param       aEpa                    Pokazivac na postojeci EPA objekat
  * @param       aStorage                memorijski prostor za red cekanja,
  * @param       aQueueSize              velicina potrebnog reda cekanja.
  * @details     Inicijalizuje strukturu i rezervise memorijski prostor za red
  *              cekanja za dati @c aEvtQueue red cekanja.
- *//*--------------------------------------------------------------------------------------------*/
-void evtQInit(
+ */
+void evtQInitI(
     esEpaHeader_T       * aEpa,
     esEvtHeader_T       ** aStorage,
     size_t              aQueueSize);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dekonstruise red cekanja za dogadjaje
  * @param       aEpa                    Pokazivac na red cekanja koji se
  *                                      dekonstruise.
  * @details     Svi dogadjaji koji su u redu cekanja ce se prikupiti i obrisati.
- *//*--------------------------------------------------------------------------------------------*/
-void evtQDeInit(
+ */
+void evtQDeInitI(
     esEpaHeader_T       * aEpa);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Dobavlja dogadjaj iz reda za cekanje @c aEvtQueue
  * @param       aEpa                    Pokazivac na red za cekanje.
  * @return      Dogadjaj iz reda cekanja.
  * @iclass
- *//*--------------------------------------------------------------------------------------------*/
+ */
 esEvtHeader_T * evtQGetI(
     esEpaHeader_T       * aEpa);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Salje dogadjaj na pocetku reda za cekanje (LIFO metod).
  * @param       aEpa                    Pokazivac na red za cekanje,
  * @param       aEvt                    pokazivac na dogadjaj koji se salje.
  * @iclass
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void evtQPutAheadI(
     esEpaHeader_T       * aEpa,
     esEvtHeader_T       * aEvt);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Salje dogadjaj na pocetku reda za cekanje (LIFO metod).
  * @param       aEpa                    Pokazivac na red za cekanje,
  * @param       aEvt                    pokazivac na dogadjaj koji se salje.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void evtQPutAhead(
     esEpaHeader_T       * aEpa,
     esEvtHeader_T       * aEvt);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Salje dogadjaj na kraju reda za cekanje (FIFO metod).
  * @param       aEpa                    Pokazivac na red za cekanje,
  * @param       aEvt                    pokazivac na dogadjaj koji se salje.
  * @iclass
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void evtQPutI(
     esEpaHeader_T       * aEpa,
     esEvtHeader_T       * aEvt);
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Salje dogadjaj na kraju reda za cekanje (FIFO metod).
  * @param       aEpa                    Pokazivac na red za cekanje,
  * @param       aEvt                    pokazivac na dogadjaj koji se salje.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 void evtQPut(
     esEpaHeader_T       * aEpa,
     esEvtHeader_T       * aEvt);
 
-/*-----------------------------------------------------------------------------------------------*
- * C/C++ #endif - close
- *-----------------------------------------------------------------------------------------------*/
-#ifdef __cplusplus
+/*---------------------------------------------------------------------------  C++ extern end  --*/
+#if defined(__cplusplus)
 }
 #endif
 
-
-/*************************************************************************************************
- * CONFIGURATION ERRORS
- *************************************************************************************//** @cond */
-
+/*===================================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
 
 /** @endcond *//** @} *//*************************************************************************
  * END of evt_pkg.h
