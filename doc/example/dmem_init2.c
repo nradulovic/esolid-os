f1ec3d0b90d2719e243bfc109a6e94e25b7bde7d
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-04-23 00:40:09 +0000

    - dodat je drugi primer

---
+++ doc/example/dmem_init2.c
@@ -0,0 +1,37 @@
+#include "kernel/mem.h"
+
+#define BUFFER_SIZE                     1024
+
+int main (
+    void) {
+
+    static esDMemDesc_T myHeap;
+    static char * buffer;
+
+    esSMemInit();                                           /* Initialize the static memory */
+                                                            /* The amount of static memory is */
+                                                            /* specified in mem_config.h file */
+    buffer = esSMemAlloc(
+        BUFFER_SIZE);                                       /* Create a buffer of 1024 bytes */
+
+    esDMemInit(
+        &myHeap,
+        buffer,
+        BUFFER_SIZE);                                       /* Initialize the dynamic memory */
+
+    while (TRUE) {
+        int * myArray;
+
+        myArray = esDMemAlloc(
+            &myHeap,
+            sizeof(int) * 10U);                             /* Allocate an array of 10 integers */
+
+        /*
+         * Do some stuff
+         */
+
+        esDMemDeAlloc(
+            &myHeap,
+            myArray);                                       /* Delete the array */
+    }
+}
